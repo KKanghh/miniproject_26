9e678a7032c973aa43c95e33b69c6af812ee6608
EllieHyun <hyunyelim25@gmail.com>
2023-04-24 23:15:39 +0900

    login page design

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import AuthContext from "../store/auth-context";
 import { Button } from "react-bootstrap";
+import "../App.css";
 
 function Auth() {
   const ctx = useContext(AuthContext);
@@ -25,31 +26,46 @@ function Auth() {
   };
   return (
     <>
-      <form>
-        <div>
-          <label htmlFor="name">이름</label>
-          <input
-            type="text"
-            name="name"
-            value={input.name}
-            onChange={inputHandler}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">비밀번호</label>
-          <input
-            type="password"
-            name="password"
-            value={input.password}
-            onChange={inputHandler}
-          />
-        </div>
-        <div>
-          <Button type="button" variant="primary" onClick={buttonClickHandler}>
-            로그인
-          </Button>
-        </div>
-      </form>
+      <div class="loginForm">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/flatly/bootstrap.min.css" integrity="sha384-qF/QmIAj5ZaYFAeQcrQ6bfVMAh4zZlrGwTPY7T/M+iTTLJqJBJjwwnsE5Y0mV7QK" crossorigin="anonymous"></link>
+        <form>
+          <h3>당신의 혼밥 친구를 찾아보세요!</h3>
+          <div class="loginBox">
+            <div class="form-group">
+              <label htmlFor="name" class="form-label mt-4">아이디</label>
+              <input
+                style={{ width: "90%", margin: "0 auto" }}
+                class="form-control"
+                type="text"
+                name="name"
+                value={input.name}
+                onChange={inputHandler}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="password">비밀번호</label>
+              <input
+                style={{ width: "90%", margin: "0 auto" }}
+                class="form-control"
+                type="password"
+                name="password"
+                value={input.password}
+                onChange={inputHandler}
+              />
+            </div>
+
+            <div class="d-grid gap-2" style={{ width: "90%", margin: "30px auto 10px auto" }}>
+              <button type="button" class="btn btn-lg btn-primary" onClick={buttonClickHandler}>Log in</button>
+
+            </div>
+
+            <div class="d-grid gap-2" style={{ width: "90%", margin: "0 auto 5% auto" }}>
+              <button type="button" class="btn btn-lg btn-secondary">Sign up</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
